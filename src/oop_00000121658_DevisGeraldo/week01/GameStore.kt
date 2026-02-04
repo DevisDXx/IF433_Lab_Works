1053dbab927a2fb825ea3b468a85cb897d1ee81e
DevisDXx <devis.g2@gmail.com>
2026-02-04 20:28:05 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000121658_DevisGeraldo/week01/GameStore.kt
+++ src/oop_00000121658_DevisGeraldo/week01/GameStore.kt
@@ -3,4 +3,13 @@ package oop_00000121658_DevisGeraldo.week01
 fun main() {
     val gameTitle = "Sonic Frontiers"
     val price = 650000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
