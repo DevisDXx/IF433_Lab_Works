00f9531ab98cd3c8117c692e9a972c127ad00805
DevisDXx <devis.g2@gmail.com>
2026-02-17 20:48:37 +0700

    week02: implement selection logic for constructors

--- src/oop_00000121658_DevisGeraldo/week02/Main.kt
+++ src/oop_00000121658_DevisGeraldo/week02/Main.kt
@@ -10,18 +10,33 @@ fun main() {
     print("Masukkan Nama: ")
     val name = scanner.nextLine()
 
-    print("Masukkan NIM (Wajib 5 karakter): ")
-    val nim = scanner.next()
-
-    scanner.nextLine()
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.nextLine()
 
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-    } else {
+        return
+    }
+
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine() // Consume newline
+
+    if (type == 1) {
         print("Masukkan Jurusan: ")
         val major = scanner.nextLine()
 
+        // Memanggil Primary Constructor
         val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+    } else if (type == 2) {
+
+        // Memanggil Secondary Constructor
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
     }
 }
