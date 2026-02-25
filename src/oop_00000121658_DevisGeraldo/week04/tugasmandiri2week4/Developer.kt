9f5565628686894682b92f4ec0e2fae315f4727f
DevisDXx <devis.g2@gmail.com>
2026-02-25 09:11:16 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_00000121658_DevisGeraldo/week04/tugasmandiri2week4/Developer.kt
@@ -0,0 +1,12 @@
+package oop_00000121658_DevisGeraldo.week04.tugasmandiri2week4
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}
