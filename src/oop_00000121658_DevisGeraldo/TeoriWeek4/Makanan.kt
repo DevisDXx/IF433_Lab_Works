045e61b37ab1471ded3d7d75d4d798f7b022a01f
DevisDXx <devis.g2@gmail.com>
2026-03-04 08:44:40 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000121658_DevisGeraldo/TeoriWeek4/Makanan.kt
@@ -0,0 +1,15 @@
+package oop_00000121658_DevisGeraldo.TeoriWeek4
+
+open class Makanan {
+    private var namaResto:String = "";
+    protected var harga:Int = 0;
+    protected var stock:Int = 10;
+
+    init {
+        namaResto = "Steak 12";
+        println("Resto saya $namaResto");
+    }
+    open public fun jual_makanan() {
+        println("Saya jual makanan");
+    }
+}
---
+++ src/oop_00000121658_DevisGeraldo/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000121658_DevisGeraldo.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
