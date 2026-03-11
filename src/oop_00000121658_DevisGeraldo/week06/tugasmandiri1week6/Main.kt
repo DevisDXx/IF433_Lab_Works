ea9b6dde8bcb55b78b607bd68a44635020a2148e
DevisDXx <devis.g2@gmail.com>
2026-03-11 15:24:54 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000121658_DevisGeraldo/week06/tugasmandiri1week6/Main.kt
@@ -0,0 +1,8 @@
+package oop_00000121658_DevisGeraldo.week06.tugasmandiri1week6
+
+fun main() {
+
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+}
