ff028f04286db1146e931dee63dfe5b3af824070
DevisDXx <devis.g2@gmail.com>
2026-03-11 14:55:09 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000121658_DevisGeraldo/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000121658_DevisGeraldo.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
