ef2d37f6bf1badeb1cd181af76d9e3f8b112ff7b
DevisDXx <devis.g2@gmail.com>
2026-03-11 15:21:46 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000121658_DevisGeraldo/week06/tugasmandiri1week6/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_00000121658_DevisGeraldo.week06.tugasmandiri1week6
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
