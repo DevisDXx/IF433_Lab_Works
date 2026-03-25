95b688396dfae7c4537c0ac07b5c53607970b10a
DevisDXx <devis.g2@gmail.com>
2026-03-25 13:02:21 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000121658_DevisGeraldo/week07/tugasmandiri1week7/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000121658_DevisGeraldo.week07.tugasmandiri1week7
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
