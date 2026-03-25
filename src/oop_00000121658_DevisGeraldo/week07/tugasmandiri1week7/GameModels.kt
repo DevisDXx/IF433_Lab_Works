7c5f2f0f1022b3651d5b1a7540805f9f47c059bf
DevisDXx <devis.g2@gmail.com>
2026-03-25 13:04:49 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000121658_DevisGeraldo/week07/tugasmandiri1week7/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000121658_DevisGeraldo.week07.tugasmandiri1week7
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
