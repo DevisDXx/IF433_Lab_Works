ea4eeddd8a6d4094fc0cebe8ab434ee690dd88ef
DevisDXx <devis.g2@gmail.com>
2026-04-15 09:31:20 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000121658_DevisGeraldo/week08/tugasmandiri1week8/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000121658_DevisGeraldo.week08.tugasmandiri1week8;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
