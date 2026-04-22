562a83838e56db7bf0794b9470220f64de23c958
DevisDXx <devis.g2@gmail.com>
2026-04-22 09:03:33 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000121658_DevisGeraldo/week09/tugasmandiri1week9/TradingDashboard.kt
+++ src/oop_00000121658_DevisGeraldo/week09/tugasmandiri1week9/TradingDashboard.kt
@@ -24,4 +24,10 @@ fun main() {
         .map {
             "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+    //5
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+        .map {
+            "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
 }
