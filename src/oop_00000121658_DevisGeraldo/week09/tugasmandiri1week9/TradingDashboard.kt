eecb9640a1e6c83aeb64a65e799a5dfe971ef870
DevisDXx <devis.g2@gmail.com>
2026-04-22 08:59:33 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000121658_DevisGeraldo/week09/tugasmandiri1week9/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000121658_DevisGeraldo.week09.tugasmandiri1week9
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 8.0, "CLOSED"),
+        TradeLog("XRPUSDT", "LONG", 5, 0.0, "CLOSED"),
+        TradeLog("ADAUSDT", "LONG", 10, 12.3, "OPEN")
+    )
+}
