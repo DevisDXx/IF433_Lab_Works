9b239ef968857d37c8cbb6b2973d48c6d65abab2
DevisDXx <devis.g2@gmail.com>
2026-04-22 08:57:09 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000121658_DevisGeraldo/week09/tugasmandiri1week9/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000121658_DevisGeraldo.week09.tugasmandiri1week9
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
