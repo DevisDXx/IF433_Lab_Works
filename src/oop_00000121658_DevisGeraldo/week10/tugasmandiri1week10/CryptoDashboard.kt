9bfc3ea280d756e9c4a7e48643c646a19ae8936e
DevisDXx <devis.g2@gmail.com>
2026-04-29 14:03:36 +0700

    week10: (task) populate coin repository C16

--- src/oop_00000121658_DevisGeraldo/week10/tugasmandiri1week10/CryptoDashboard.kt
+++ src/oop_00000121658_DevisGeraldo/week10/tugasmandiri1week10/CryptoDashboard.kt
@@ -3,4 +3,8 @@ package oop_00000121658_DevisGeraldo.week10.tugasmandiri1week10
 fun main() {
 
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
