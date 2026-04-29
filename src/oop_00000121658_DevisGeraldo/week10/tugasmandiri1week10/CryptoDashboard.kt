15776aa7094c7fcd2a338ba7250c935c03d66949
DevisDXx <devis.g2@gmail.com>
2026-04-29 14:06:14 +0700

    week10: (task) initialize and populate transaction repository C19

--- src/oop_00000121658_DevisGeraldo/week10/tugasmandiri1week10/CryptoDashboard.kt
+++ src/oop_00000121658_DevisGeraldo/week10/tugasmandiri1week10/CryptoDashboard.kt
@@ -14,4 +14,14 @@ fun main() {
     response.data.forEach {
         println("Coin: ${it.name}, Balance: ${it.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+
+    txRepo.add(Transaction("TX001", 500.0))
+    txRepo.add(Transaction("TX002", 250.0))
+
+    println("\nTransactions:")
+    txRepo.getAll().forEach {
+        println("ID: ${it.id}, Amount: ${it.amount}")
+    }
 }
