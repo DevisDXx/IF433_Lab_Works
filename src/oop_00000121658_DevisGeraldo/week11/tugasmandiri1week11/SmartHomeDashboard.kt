3492b7a33867310fc29c860f45f1290e5372b1e0
DevisDXx <devis.g2@gmail.com>
2026-05-06 13:27:45 +0700

    week11: (task) calculate total power usage C19

--- src/oop_00000121658_DevisGeraldo/week11/tugasmandiri1week11/SmartHomeDashboard.kt
+++ src/oop_00000121658_DevisGeraldo/week11/tugasmandiri1week11/SmartHomeDashboard.kt
@@ -39,4 +39,9 @@ fun main() {
         println("Jumlah perangkat: ${this.size}")
     }
 
+    val totalPower = homeDevices.run {
+        sumOf { it.powerLoad }
+    }
+    println("Total Daya: $totalPower Watt")
+
 }
