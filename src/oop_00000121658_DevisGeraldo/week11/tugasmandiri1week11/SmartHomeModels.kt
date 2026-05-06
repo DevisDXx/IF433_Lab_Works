9e823c4bbac338581646187795b82bc8b01431bf
DevisDXx <devis.g2@gmail.com>
2026-05-06 09:38:14 +0700

    week11: (task) create SmartDevice data class C11

---
+++ src/oop_00000121658_DevisGeraldo/week11/tugasmandiri1week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000121658_DevisGeraldo.week11.tugasmandiri1week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
