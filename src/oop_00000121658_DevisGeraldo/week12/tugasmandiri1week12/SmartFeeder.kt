b245fda385273d2a9e8edcf4f4422b857401bae3
DevisDXx <devis.g2@gmail.com>
2026-05-13 09:30:06 +0700

    week12: (task) execute feeding using runCatching C18

--- src/oop_00000121658_DevisGeraldo/week12/tugasmandiri1week12/SmartFeeder.kt
+++ src/oop_00000121658_DevisGeraldo/week12/tugasmandiri1week12/SmartFeeder.kt
@@ -67,3 +67,7 @@ fun main() {
             isJammed = false
         )
     }
+        .onSuccess { newStock ->
+            currentKibbleStock = newStock
+            println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+        }
