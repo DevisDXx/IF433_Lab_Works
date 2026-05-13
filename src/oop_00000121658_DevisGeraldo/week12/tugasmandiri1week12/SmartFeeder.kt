06255aabb8f48b451be69e9a67c74ab66c0fa58d
DevisDXx <devis.g2@gmail.com>
2026-05-13 09:22:37 +0700

    week12: (task) implement hardware validation with throw C12

--- src/oop_00000121658_DevisGeraldo/week12/tugasmandiri1week12/SmartFeeder.kt
+++ src/oop_00000121658_DevisGeraldo/week12/tugasmandiri1week12/SmartFeeder.kt
@@ -5,8 +5,16 @@ fun dispenseKibble(
     availableGram: Int,
     isJammed: Boolean
 ): Int {
-
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
 }
