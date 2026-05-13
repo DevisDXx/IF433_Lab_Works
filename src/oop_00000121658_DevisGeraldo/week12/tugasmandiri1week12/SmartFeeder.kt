cd6ae03ba637b536459d11556824a80abe39dc7d
DevisDXx <devis.g2@gmail.com>
2026-05-13 09:21:39 +0700

    week12: (task) implement dispenseKibble function C11

---
+++ src/oop_00000121658_DevisGeraldo/week12/tugasmandiri1week12/SmartFeeder.kt
@@ -0,0 +1,12 @@
+package oop_00000121658_DevisGeraldo.week12.tugasmandiri1week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+}
