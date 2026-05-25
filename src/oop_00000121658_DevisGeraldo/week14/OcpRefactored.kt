97eee97fdedcb27d6f928eba17aaac5ee8e7b02a
DevisDXx <devis.g2@gmail.com>
2026-05-25 09:08:36 +0700

    week14: create DiscountStrategy interface and implementations C6

---
+++ src/oop_00000121658_DevisGeraldo/week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_00000121658_DevisGeraldo.week14
+
+interface DiscountStrategy {
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.80
+}
+
+class memberDiscount: DiscountStrategy {
+    override fun apply(price: Double) = price * 0.85
+}
